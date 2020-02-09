548f01e2097b5b618383292faff1a4697fee7330
Sebin P Johnson <1468974-sebinpj@users.noreply.gitlab.com>
2020-02-09 09:47:12 +0530

    SEBIN | WESABOO | d3 and webpack sourcemaps

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,34 @@
+import * as d3 from 'd3';
 
-function component() {
-    let element = document.createElement('div');
-    let array = [1,2,3].map((e) => e ** 2)
-    element.innerHTML = array.join(' ');
-    return element;
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
 
-$('.test-div').append(component());
 
-$('.test-div').on('click', function () {
-    alert('yup');
-})
+d3.json('data/buildings.json').then((data) => {
+  const svg = d3.select("#canvas").append("svg")
+    .attr("width", 400)
+    .attr("height", 600);
+  for(let d of data){
+    d.height = +d.height;
+  }
+  const rectangles = svg.selectAll('rect')
+    .data(data);
+
+  rectangles.enter()
+    .append('rect')
+    .attr('width', 40)
+    .attr('height',(d) => {
+      console.log(d)
+      return d.height;
+    })
+    .attr('y',0)
+    .attr('x', (d,i)  => 50*i)
+    .attr('fill',()=>getRandomColor())
+});
--- webpack.config.js
+++ webpack.config.js
@@ -2,84 +2,89 @@ const path = require('path');
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: [
-        './src/index.ejs',
-        './assets/js/index.js',
-        './assets/scss/style.scss',
-    ],
-    mode: 'development',
-    output: {
-        filename: 'js/main.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    plugins: [
-        new webpack.ProvidePlugin({
-            $: 'jquery'
-        }),
-        new MiniCssExtractPlugin({
-            filename: "css/style.css"
-        }),
-        new OptimizeCssAssetsPlugin({
-            assetNameRegExp: /\.css$/g,
-            cssProcessor: require('cssnano'),
-            cssProcessorPluginOptions: {
-                preset: ['default', {
-                    discardComments: {
-                        removeAll: true
-                    }
-                }],
+  entry: [
+    './src/index.ejs',
+    './assets/js/index.js',
+    './assets/scss/style.scss',
+  ],
+  mode: 'development',
+  output: {
+    filename: 'js/main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery'
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/style.css"
+    }),
+    new CopyPlugin([
+      {from: 'assets/data', to: 'dist/data'},
+    ]),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorPluginOptions: {
+        preset: ['default', {
+          discardComments: {
+            removeAll: true
+          }
+        }],
+      },
+      canPrint: true
+    })
+  ],
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  },
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [
+        MiniCssExtractPlugin.loader,
+        "css-loader",
+        "sass-loader"
+      ]
+    }, {
+      test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    }, {
+      test: /\.ejs$/,
+      use: [{
+        loader: 'file-loader',
+        options: {
+          name: 'index.html',
+          context: './src/',
+          outputPath: ''
+        }
+      },
+        {
+          loader: 'extract-loader'
+        },
+        {
+          loader: "ejs-webpack-loader",
+          options: {
+            data: {
+              title: "New Title",
+              someVar: "hello world"
             },
-            canPrint: true
-        })
-    ],
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 9000
-    },
-    module: {
-        rules: [{
-            test: /\.scss$/,
-            use: [
-                MiniCssExtractPlugin.loader,
-                "css-loader",
-                "sass-loader"
-            ]
-        }, {
-            test: /\.m?js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env']
-                }
-            }
-        }, {
-            test: /\.ejs$/,
-            use: [{
-                    loader: 'file-loader',
-                    options: {
-                        name: 'index.html',
-                        context: './src/',
-                        outputPath: ''
-                    }
-                },
-                {
-                    loader: 'extract-loader'
-                },
-                {
-                    loader: "ejs-webpack-loader",
-                    options: {
-                        data: {
-                            title: "New Title",
-                            someVar: "hello world"
-                        },
-                        htmlmin: true
-                    }
-                }
-            ]
-        }]
-    }
+            htmlmin: true
+          }
+        }
+      ]
+    }]
+  }
 };
